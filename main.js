e3b4b8e5fb933b8b2c38bc1e8c1a9bc2653bac1a
Johann Dee <johanndee1994@gmail.com>
2023-08-03 21:41:22 -0700

    Change 'prompt' to 'result'

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ document.addEventListener('DOMContentLoaded', function () {
 // Query Selectors
 var playerScore = document.querySelector(".player-score");
 var computerScore = document.querySelector(".computer-score");
-var prompt = document.querySelector(".status"); // status
-var changeGameBtn = document.querySelector(".change-game"); // button
+var result = document.querySelector(".result");
+var changeGameBtn = document.querySelector(".change-game");
 var intro = document.querySelector(".intro");
 var choose = document.querySelector(".choose");
 var main = document.querySelector("main");
